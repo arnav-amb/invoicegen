8edfc2bd0e97b6c2e28e17c8a4baf395463cbf02
Arnav <arnav.kr.grv@gmail.com>
2019-07-20 12:01:41 +0000

    Quantity precision fix

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function generate(doprint) {
 	xroundtot = parseFloat(xroundtot).toFixed(2);
 	var xadv = parseFloat(fx["lessadv"].value);
 	var gross = parseFloat((xroundtot - xadv)).toFixed(2);
-	doc.text(String(parseFloat(totquant).toFixed(2)), 131.1,208,optionstxt);
+	doc.text(String(parseFloat(totquant).toFixed(3)), 131.1,208,optionstxt);
 	doc.text(String(xcout.toFixed(2)), 187, 208,optionstxt);
 	doc.text(String(xsgst.toFixed(2)), 187, 214,optionstxt);
 	doc.text(String(xcgst.toFixed(2)), 187, 220.5,optionstxt);
