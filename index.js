325ab3fc3e4ef4ceb66594850da3eb2893d5b66a
Soumya Sinha <42693549+Soumyasinha29@users.noreply.github.com>
2021-11-12 00:36:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,9 +67,9 @@ function generate(doprint) {
 	var gross = parseFloat((xroundtot - xadv)).toFixed(2);
 	doc.text(String(parseFloat(totquant).toFixed(3)), 131.1,208,optionstxt);
 	doc.text(String(xcout.toFixed(2)), 187, 208,optionstxt);
-	doc.text(String(xsgst.toFixed(2)), 187, 228,optionstxt);
-	doc.text(String(xcgst.toFixed(2)), 187, 230.5,optionstxt);
-	doc.text(String(xigst.toFixed(2)), 187, 236.5,optionstxt);
+	doc.text(String(xsgst.toFixed(2)), 187, 224,optionstxt);
+	doc.text(String(xcgst.toFixed(2)), 187, 227.5,optionstxt);
+	doc.text(String(xigst.toFixed(2)), 187, 233.5,optionstxt);
 	doc.text(String(xother.toFixed(2)), 187, 216.5,optionstxt);
 	doc.text(String(xroundtot), 187, 245,optionstxt);
 	doc.text(String(xadv.toFixed(2)), 187, 253.5,optionstxt);
